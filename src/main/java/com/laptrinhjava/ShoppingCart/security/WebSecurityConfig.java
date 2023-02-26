773cc319377ea3b817d1738521d1c8294c02b922
khanh1902 <khanhdt.game@gmail.com>
2023-02-26 15:37:50 +0700

    main

--- src/main/java/com/laptrinhjava/ShoppingCart/security/WebSecurityConfig.java
+++ src/main/java/com/laptrinhjava/ShoppingCart/security/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://shopping-cart-ofji.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
